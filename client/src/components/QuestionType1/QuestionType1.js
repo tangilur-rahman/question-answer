2d1e74751aa150da12e72d8df1698ebf73c94ae7
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-12-23 03:53:10 +0600

    first question set-up done

---
+++ client/src/components/QuestionType1/QuestionType1.js
@@ -0,0 +1,14 @@
+// external components
+
+// internal components
+import "./QuestionType1.css";
+
+const QuestionType1 = () => {
+	return (
+		<>
+			<h1>hi</h1>
+		</>
+	);
+};
+
+export default QuestionType1;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,29 @@
 // external components
 
 // internal components
+import QuestionType1 from "../../components/QuestionType1/QuestionType1";
 import "./Home.css";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<>
+			<div className="container-fluid p-0">
+				<div className="row m-0 home-container">
+					<div className="col-10 p-0 home-wrapper">
+						<header>Question & Answer</header>
+						<div className="question-section" id="first-ques">
+							<span>1.</span>
+							<QuestionType1 />
+						</div>
+						<div className="question-section">
+							<span>1.</span>
+							<QuestionType1 />
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Home;
